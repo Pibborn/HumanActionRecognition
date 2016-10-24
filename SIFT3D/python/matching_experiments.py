a0d194a13b61fff7733d2e0063f5e271ab0cddc6
Mattia Cerrato <mattia.cerrato@studenti.unito.it>
2016-10-24 10:20:00 +0200

    Move infer_label function in separate utils file

--- SIFT3D/python/matching_experiments.py
+++ SIFT3D/python/matching_experiments.py
@@ -4,47 +4,13 @@ import csv
 import numpy as np
 import collections
 from SIFT3D.python.settings import Constants
+from .utils import infer_label
 import logging
 logging.basicConfig(level=Constants.LOGGING_LEVEL)
 from sklearn import svm, neighbors
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-
-def infer_label(file_name):
-    file_name = file_name
-    if "boxing" in file_name:
-        return Constants.BOXING_LABEL
-    if "running" in file_name:
-        return Constants.RUNNING_LABEL
-    if "jogging" in file_name:
-        return Constants.JOGGING_LABEL
-    if "walking" in file_name:
-        return Constants.WALKING_LABEL
-    if "handclapping" in file_name:
-        return Constants.HANDCLAPPING_LABEL
-    if "handwaving" in file_name:
-        return Constants.HANDWAVING_LABEL
-    if "bend" in file_name:
-        return Constants.BEND_LABEL
-    if "jack" in file_name:
-        return Constants.JACK_LABEL
-    # it is important that this check comes before the 'jump' one
-    if "pjump" in file_name:
-        return Constants.PJUMP_LABEL
-    if "jump" in file_name:
-        return Constants.JUMP_LABEL
-    if "run" in file_name:
-        return Constants.WEISSMAN_RUN_LABEL
-    if "side" in file_name:
-        return Constants.SIDE_LABEL
-    if "skip" in file_name:
-        return Constants.SKIP_LABEL
-    if "walk" in file_name:
-        return Constants.WEISSMAN_WALK_LABEL
-    if "wave" in file_name:
-        return Constants.WEISSMAN_WAVE_LABEL
-    return -1
 
 def load_samples_from_file(file_name, X, y):
     csvReader = csv.reader(open(file_name, "r"))
