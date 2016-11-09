b5626cc63620d01e679a5a848687fea8d1fd4cd3
Yash Garg <ygarg@asu.edu>
2016-11-09 13:53:19 -0700

    Added SVM Library

--- Results/scriptSVM.m
+++ Results/scriptSVM.m
@@ -1,53 +1,61 @@
 clear;
 clc;
 
-basePath = 'C:\Users\ygarg\Dropbox (ASU)\KTH Results\sift3D\Results\Weizmann\';
-vocabPath = 'C:\Users\ygarg\Dropbox (ASU)\KTH Results\sift3D\Results\Weizmann\Vocabulary\';
+%% Initializing the Paths
+dataPath = strcat(pwd,'\Weizmann\');
+vocabPath = strcat(pwd,'\Weizmann\SIFT3DDesVocab\');
+% vocabPath = 'C:\Users\ygarg\Dropbox (ASU)\sift3D\Results\Weizmann\Vocabulary\';
+%% INITIALIZING THE VACABULARY
 vocabs = [50, 100, 250, 500, 750, 1000, 1250, 1500];
-vocabType = '_SIFT3Des.csv';
+nVocabs = length(vocabs);
+vocabType = 'Multi_XYT_DES_';
 
-trainInd = csvread(strcat(basePath,'trainInd.csv'));
-testInd  = csvread(strcat(basePath,'testInd.csv'));
-labels   = csvread(strcat(basePath,'labels.csv'));
+%% INITIALIZING THE TRAINING AND TESTING DATA
+trainInd = csvread(strcat(dataPath,'trainInd.csv'));
+testInd  = csvread(strcat(dataPath,'testInd.csv'));
+labels   = csvread(strcat(dataPath,'labels.csv'));
 
-% labels   = csvread('C:\Users\reeyash\Dropbox (ASU)\MPEG\code\sift3D\SIFT3D Results\labels.csv');
+NumofClass = length(unique(labels));
+nIterations = size(trainInd, 2);
 
-NumofClass = 10;
-
-scores = zeros(10, 8);
-for v = 1 : 8
-    vocab = vocabs(v);
-    data = csvread(strcat(vocabPath,num2str(vocab),vocabType));
-    fprintf('Vocab Size : %d\n', vocab);
-    for i = 1 : 10
-        fprintf('(Iteration : %d\n', i);
-        trainData = data(trainInd(:,i),:);
-        
-        subTrainData = bsxfun(@minus,trainData,min(trainData));
-        trainData = bsxfun(@rdivide,subTrainData,max(trainData)-min(trainData));
-        
-        testData = data(testInd(:,i),:);
-        
-        subTestData = bsxfun(@minus,testData,min(testData));
-        testData = bsxfun(@rdivide,subTestData,max(testData)-min(testData));
-        
-        trainLbl = labels(trainInd(:,i));
-        testLbl = labels(testInd(:,i));
-        
-        model = cell(NumofClass,1);
-        for k = 1:NumofClass
-            model{k} = svmtrain(double(trainLbl==k), trainData, '-t 0 -h 0 -b 1');
+accuracy = zeros(2,nVocabs);
+for kernel = 1 : 2
+    scores = zeros(nIterations, nVocabs);
+    for v = 1 : nVocabs
+        vocab = vocabs(v);
+        data  = csvread(strcat(vocabPath,vocabType,num2str(vocab),'.csv'));
+        % data  = csvread(strcat(vocabPath,num2str(vocab),'_SIFT3Des.csv'));
+        fprintf('Vocab Size : %d\n', vocab);
+        for i = 1 : nIterations
+            fprintf('(Iteration : %d\n', i);
+            trainData = data(trainInd(:,i),:); 
+            testData = data(testInd(:,i),:);
+            
+            trainLbl = labels(trainInd(:,i));
+            testLbl  = labels(testInd(:,i));
+            
+            model = cell(NumofClass,1);
+            for k = 1:NumofClass
+                if kernel == 1
+                    % Linear Kernel
+                    model{k} = svmtrain(double(trainLbl==k), trainData, '-t 0 -h 0 -b 1');
+                else
+                    % Polynomial with degree 2
+                    model{k} = svmtrain(double(trainLbl==k), trainData, '-t 1 -d 2 -h 0 -b 1');
+                end
+            end
+            
+            % calculate the probability of different labels
+            pr = zeros(length(testLbl),NumofClass);
+            for k = 1:NumofClass
+                [~,~,p] = svmpredict(double(testLbl==k), testData, model{k}, '-b 1');
+                pr(:,k) = p(:,model{k}.Label==1);    %# probability of class==k
+            end
+            
+            [~,predictedLabel] = max(pr,[],2);
+            scores(i, v) = (sum(logical((predictedLabel-testLbl)==0))/NumofClass)*100;
         end
-        
-        % calculate the probability of different labels
-        pr = zeros(length(testLbl),NumofClass);
-        for k = 1:NumofClass
-            [~,~,p] = svmpredict(double(testLbl==k), testData, model{k}, '-b 1');
-            pr(:,k) = p(:,model{k}.Label==1);    %# probability of class==k
-        end
-        
-        [~,predictedLabel] = max(pr,[],2);
-        scores(i, v) = (sum(logical((predictedLabel-testLbl)==0))/NumofClass)*100;
     end
+    accuracy(kernel,:) = mean(scores);
 end
-accuracy = mean(scores)
+accuracy
