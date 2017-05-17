f66aabc23683de4a5f61e678ada70b18100763f5
Mattia Cerrato <mattia.cerrato@studenti.unito.it>
2017-05-17 11:33:29 +0200

    descriptor_polling: Handle keras models properly

--- SIFT3D/python/matching/descriptor_polling.py
+++ SIFT3D/python/matching/descriptor_polling.py
@@ -9,6 +9,7 @@ from SIFT3D.python.utils.utils import *
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, BatchNormalization, Merge
 from keras.regularizers import l2
+from keras.utils.np_utils import to_categorical
 
 # TODO: uniformare la signature dei modelli keras e di quelli scikit-learn. In questo momento
 # bisogna modificare majority_voting per usare uno piuttosto che l'altro.
@@ -41,7 +42,6 @@ def neural_net_model(X, y):
     :param y: np.ndarray
     :return: model: keras neural network
     """
-    X -= np.mean(X)
     model = Sequential()
     model.add(Dense(260, input_dim=768, init='glorot_normal', W_regularizer=l2(1e-3)))
     model.add(BatchNormalization())
@@ -74,7 +74,6 @@ def neural_net_model_quadrants(X, y):
     :param y: np.ndarray
     :return: model: keras neural network
     """
-    X -= np.mean(X)
     # check for correct X shape, or we won't be able to build the input layer
     assert np.shape(X)[1] % 8 == 0
     quadrant_size = np.shape(X)[1] / 8
@@ -98,21 +97,30 @@ def neural_net_model_quadrants(X, y):
                   metrics=['accuracy'])
     return model
 
-def train_descriptor_prediction_model(X, y):
+def train_descriptor_prediction_model(X, y, **kwargs):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, stratify=y)
     X_train -= np.mean(X_train)
     X_test -= np.mean(X_test)
     # You can use any model you wish. Some examples are included. KNeighbors usually works best.
-    # model = svm.SVC().fit(X_train, y_train)
-    model = neighbors.KNeighborsClassifier(15)
-    logging.info("Training the descriptor polling model")
-    logging.info("Using " + str(type(model)) + " as a descriptor-level model.")
-    model.fit(X_train, y_train)
-    # model = svm.LinearSVC(C=1e-06, max_iter=2000, verbose=1, tol=0.0000001).fit(X_train, y_train)
-    accuracy = model.score(X_train, y_train)
-    logging.info("Achieved " + str(accuracy) + " accuracy on the training set")
-    accuracy = model.score(X_test, y_test)
-    logging.info("Achieved " + str(accuracy) + " accuracy on the test set")
+    # model = svm.SVC()
+    # model = svm.LinearSVC(C=1e-06, max_iter=2000, verbose=1, tol=0.0000001)
+    model = neural_net_model(X_train, y_train)
+    # model = neighbors.KNeighborsClassifier(15)
+
+    # handle the keras model case, which requires different labels (but with the same meaning)
+    # and has a different fit() function signature
+    if 'keras' in str(type(model)):
+        y_train = to_categorical(y_train)
+        y_test = to_categorical(y_test)
+        model.fit(X_train, y_train, **kwargs)
+        train_accuracy = model.evaluate(X_train, y_train)
+        test_accuracy = model.evaluate(X_test, y_test)
+    else:
+        model.fit(X_train, y_train)
+        train_accuracy = model.score(X_train, y_train)
+        test_accuracy = model.evaluate(X_test, y_test)
+    logging.info("Achieved " + str(train_accuracy) + " accuracy on the training set")
+    logging.info("Achieved " + str(test_accuracy) + " accuracy on the test set")
     return model
 
 def majority_voting(model, test_videos):
@@ -130,7 +138,12 @@ def majority_voting(model, test_videos):
         y_predicted = model.predict(X)
         # the predicted label for the whole video
         # is the one that is predicted the most times for single descriptors from that video
-        max_counts = max(np.bincount(y_predicted))
+        try:
+            max_counts = max(np.bincount(y_predicted))
+        # this happens when using a keras model, again because of the labels
+        except ValueError:
+            y_predicted = np.argmax(y_predicted, axis=1)
+            max_counts = max(np.bincount(y_predicted))
         # casting to list has to be done because numpy arrays and lists are handled differently
         predicted_label = list(np.bincount(y_predicted)).index(max_counts)
         if predicted_label == actual_label:
@@ -139,10 +152,12 @@ def majority_voting(model, test_videos):
             noes += 1
     return yeah, noes
 
-def polling_experiment(dataset_id):
+def polling_experiment(dataset_id, **kwargs):
     """
     This is the main entry point for descriptor polling experiments.
     :type dataset_id: str
+    :type **kwargs: keyword args. Keras uses a different model.fit() function that also asks for verbosity
+    and number of epochs. Try this with verbosity=1 and nb_epoch=10.
     """
     if dataset_id not in Constants.DATASETS:
         logging.error('Unrecognized dataset for polling experiment. Possible datasets: ' + str(Constants.DATASETS))
@@ -152,21 +167,23 @@ def polling_experiment(dataset_id):
     logging.info("Starting a matching experiment on the "+ dataset_id + " dataset.")
     train_videos, test_videos = split_train_test_videos(dataset_path, 10)
     X_train, y_train = load_descriptors(train_videos)
-    model = train_descriptor_prediction_model(X_train, y_train)
+    model = train_descriptor_prediction_model(X_train, y_train, **kwargs)
     yeah, noes = majority_voting(model, test_videos)
     logging.info("By majority voting: " + str(yeah) + " correct predictions, " + str(noes) + " wrong predictions.")
     return yeah, noes
 
-def polling_experiment_multiple(dataset_id, times):
+def polling_experiment_multiple(dataset_id, times, **kwargs):
     """
     A simple function that executes polling_experiment(dataset_id) multiple times.
     :type dataset_id: str
     :type times: int
+    :type **kwargs: keyword args. Keras uses a different model.fit() function that also asks for verbosity
+    and number of epochs. Try this with verbosity=1 and nb_epoch=10.
     """
     total_yeahs = 0
     total_noes = 0
     for i in range(0, times):
-        yeah, noes = polling_experiment(dataset_id)
+        yeah, noes = polling_experiment(dataset_id, **kwargs)
         total_yeahs += yeah
         total_noes += noes
     logging.info("After "+str(times)+" "+ str(dataset_id) + " experiments:")
@@ -177,4 +194,4 @@ def polling_experiment_multiple(dataset_id, times):
 # Suggestion: don't put complicated experiment scripts here. Instead, if you make some changes
 # use this space to test that functionality.
 if __name__ == "__main__":
-    polling_experiment_multiple('weissman', 1)
+    polling_experiment_multiple('bbrister', 1, nb_epoch=5, verbose=1)
